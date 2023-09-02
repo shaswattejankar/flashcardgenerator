dfd6fb58d2b01953dcfd0520acec09c94c067a79
shaswattejankar <shaswattejankar+github@gmail.com>
2023-09-02 23:48:51 +0530

    more cleaning

--- src/pages/flashcard_details/CarouselItem.jsx
+++ src/pages/flashcard_details/CarouselItem.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const CarouselItem = ({ci ,cd})=>{
     return(
         <div className="item" >
-            {/* <div>carousel-item component</div> */}
             <img className="carousel-img" src={ci} alt="nothing to show mf" />
             <div className="carousel-item-text">{cd}</div>
         </div>
